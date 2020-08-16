51db08ee41ee9851341b0534da79fc933479ff7e
Abdulateef Al-radaee <abdulateef.radaee@gmail.com>
2020-08-16 13:46:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+  let result = [];
+  for(const element of sourceArray) {
+    result.push(parseInt(sourceArray[element]) * -1);
+  }
+  return result;
+}
+
+mapToNoChange(sourceArray) {
+  let result = [];
+  for(const element of sourceArray) {
+    result.push(parseInt(sourceArray[element]));
+  }
+  return result;
+}
